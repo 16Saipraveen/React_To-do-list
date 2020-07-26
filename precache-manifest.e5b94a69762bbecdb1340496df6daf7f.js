d58e75053354eb55a834509757f43c0e781fd437
Sai Praveen <saipraveenchoragudi@gmail.com>
2020-07-26 20:39:09 +0530

    Updates

---
+++ precache-manifest.e5b94a69762bbecdb1340496df6daf7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1d98b4fced2f2ae46b31cf8e410c55",
+    "url": "/React_To-do-list/index.html"
+  },
+  {
+    "revision": "51d603c6df6db7fe3ad8",
+    "url": "/React_To-do-list/static/css/main.6ce00b66.chunk.css"
+  },
+  {
+    "revision": "d2092efcdc42db7c2666",
+    "url": "/React_To-do-list/static/js/2.1cb42869.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_To-do-list/static/js/2.1cb42869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d603c6df6db7fe3ad8",
+    "url": "/React_To-do-list/static/js/main.f84d7fab.chunk.js"
+  },
+  {
+    "revision": "1a1c9c1c22c02167ca95",
+    "url": "/React_To-do-list/static/js/runtime-main.4ed2a397.js"
+  },
+  {
+    "revision": "6ec428d510020d3be4e0255fac0361ac",
+    "url": "/React_To-do-list/static/media/bgimg.6ec428d5.jpg"
+  }
+]);
